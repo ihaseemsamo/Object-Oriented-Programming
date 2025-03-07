c35f47e6f350d7cb560ab5d633187d6ab2b52e7c
ihaseemsamo <haseemishere@gmail.com>
2025-03-07 15:24:17 +0500

    Update TASK 3.cpp

--- OOP LAB 05/TASK 3.cpp
+++ OOP LAB 05/TASK 3.cpp
@@ -1,92 +1,68 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Person {
-	protected:
-	    string name;
-	    int age;
-	
-	public:
-	    Person(string pName, int pAge) {
-	        name = pName;
-	        age = pAge;
-	    }
-	
-	    void displayDetails() {
-	        cout << "Name: " << name << endl;
-	        cout << "Age: " << age << endl;
-	    }
+class Employee {
+public:
+    string empName;
+    int empID;
+    string designation;
+
+    Employee(string name, int ID, string desig) : empName(name), empID(ID), designation(desig) {}
+
+    void display() const {
+        cout << "Employee ID: " << empID << endl 
+             << "Employee Name: " << empName << endl
+             << "Designation: " << designation << endl;
+    }
 };
 
-class Teacher : virtual public Person {
-	protected:
-	    string subject;
-	
-	public:
-	    Teacher(string pName, int pAge, string tSubject)
-	        : Person(pName, pAge) {
-	        subject = tSubject;
-	    }
-	
-	    void displayDetails() {
-	        cout << "Subject: " << subject << endl;
-	    }
-};
+class Project {
+private:
+    string ProjectTitle;
+    string deadline;
+    Employee* employees[10];
+    int empCount;
 
-class Researcher : virtual public Person {
-	protected:
-	    string researchArea;
-	
-	public:
-	    Researcher(string pName, int pAge, string rArea)
-	        : Person(pName, pAge) {
-	        researchArea = rArea;
-	    }
-	
-	    void displayDetails() {
-	        cout << "Research Area: " << researchArea << endl;
-	    }
-};
+public:
+    Project(string title, string dueDate) : ProjectTitle(title), deadline(dueDate), empCount(0) {}
 
-class Professor : public Teacher, public Researcher {
-	private:
-	int publications;
-	
-	public:
-	Professor(string pName, int pAge, string tSubject, string rArea, int pPublications)
-	    : Person(pName, pAge), Teacher(pName, pAge, tSubject), Researcher(pName, pAge, rArea) {
-	    publications = pPublications;
-	}
-	
-	void displayDetails() {
-	    Person::displayDetails();
-	    Teacher::displayDetails();
-	    Researcher::displayDetails();
-	    cout << "Publications: " << publications << endl;
-	}
+    void addEmployee(Employee* employee) {
+        if (empCount < 10) {
+            employees[empCount++] = employee;
+        }
+    }
+
+    void display() const {
+        cout << "Project Title: " << ProjectTitle << endl
+             << "Deadline: " << deadline << endl
+             << "Assigned Employees:\n";
+        for (int i = 0; i < empCount; i++) {
+            employees[i]->display();
+            cout << "-----------------------\n";
+        }
+        cout << endl;
+    }
 };
 
 int main() {
-    string name, subject, researchArea;
-    int age, publications;
-
-    cout << "Enter professor's name: ";
-    getline(cin, name);
-    cout << "Enter professor's age: ";
-    cin >> age;
-    cin.ignore();
-    cout << "Enter subject expertise: ";
-    getline(cin, subject);
-    cout << "Enter research area: ";
-    getline(cin, researchArea);
-    cout << "Enter number of publications: ";
-    cin >> publications;
-
-    Professor prof(name, age, subject, researchArea, publications);
-
-    cout << "\nProfessor Details:\n";
-    prof.displayDetails();
-
+    Employee e1("Ahmed", 1, "Developer");
+    Employee e2("Haseem", 2, "Manager");
+    Employee e3("Saad", 3, "Developer");
+    
+    Project p1("AI Project", "2025-06-01");
+    Project p2("CyberSecurity Project", "2025-06-01");
+    Project p3("Data Science", "2025-06-01");
+    p1.addEmployee(&e1);
+    p1.addEmployee(&e2);
+    p2.addEmployee(&e1);
+    p2.addEmployee(&e2);
+    p3.addEmployee(&e3);
+	p2.addEmployee(&e3); 
+    
+    cout << "Project Details:\n";
+    p1.display();
+    p2.display();
+    p3.display();
+    
     return 0;
 }
